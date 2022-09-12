d5350e20b0fc623c34386354feda226bf2991f5c
Anders Jonsson <aj224rj@student.lnu.se>
2022-09-12 23:24:53 +0200

    Refactor to validator-class and implement getCollectionOfDataWithPercent

--- stats/Stats.js
+++ stats/Stats.js
@@ -5,42 +5,81 @@
  * @version 1.0.0
  */
 
+import { Validator } from "../helpers/Validator"
+
+const validator = new Validator()
+
 /**
  * Wrapper class for stats-methods.
  *
+ * @export
+ * @class Stats
  */
 export class Stats {
   #collectionOfData = []
 
-  constructor (collectionOfData) {
-
-  }
-
-  getCollectionInPercent () {
-
-  }
-
-  #getSumOfCollectionOfData () {
-    // NOTE: Calculate sum of data.
-  }
-
   /**
-   * Check if data in #collectionOfData is an object with value-property.
-   * 
-   * @param {object} data - Data to verify as object with value-property.
-   * @returns {boolean}
-   */
-  #isObjectWithValueProperty (data) {
-    return (typeof data === 'object' && data.isArray()) && Object.keys(data).includes('value')
-  }
-
-  /**
-   * Check if all data in #collectionOfData is a number.
+   * The constructor of the class.
    *
-   * @param {number} - Data to verify as number.
-   * @returns {boolean}
+   * @param {number[] | object[]} listOfData - The list of data to get stats from.
+   * @throws {TypeError} - If argument is not an array of objects with value-property or an array of numbers.
    */
-  #isNumber (data) {
-    return typeof data === 'number' && !Number.isNaN(data)
+  constructor (listOfData) {
+    if (!validator.isAllNumbersOrObjectsWithValueProperty(listOfData)) {
+      throw new TypeError('Expected argument to be an array of objects with value-property or an array of numbers.')
+    }
+
+    this.#collectionOfData = [ ...collectionOfData ]
+  }
+
+  /**
+   * Get a copy of the collectionOfData-field converted to objects with percent-property.
+   *
+   * @returns {object[]} - Data from collectionOfData-field converted to objects with percent-property.
+   */
+  getCollectionOfDataWithPercent () {
+    const percentCollection = []
+    const sumOfCollection = this.#getSumOfCollectionOfData()
+
+    for (const data of this.#collectionOfData) {
+      const dataObject = this.#convertToObjectWithPercentProperty(data)
+      dataObject.percent = (dataObject.value / sumOfCollection)
+    }
+
+    return percentCollection
+  }
+
+  /**
+   * Create an object with at least a value- and percent-property.
+   *
+   * @param {object | number} data - Single data from collectionOfData-field.
+   * @returns {object} - Object with at least a value- and percent-property.
+   */
+  #convertToObjectWithPercentProperty (data) {
+    let dataObjectWithPercentProperty
+    if (this.#isObjectWithValueProperty(data)) {
+      dataObjectWithPercentProperty = { ...data, percent: undefined }
+    } else {
+      dataObjectWithPercentProperty = { value: data, percent: undefined }
+    }
+
+    return dataObjectWithPercentProperty
+  }
+
+  /**
+   * Get the sum of the values in the collectionOfData-field.
+   *
+   * @returns {number} - The sum of the values in the collectionOfData-field.
+   */
+  #getSumOfCollectionOfData () {
+    const sum = this.#collectionOfData.reduce(previousData, currentData => {
+      if (this.#isObjectWithValueProperty(currentData)) {
+        return previousData + currentData.value
+      } else {
+        return previousData + currentData
+      }
+    }, 0)
+
+    return sum
   }
 }
