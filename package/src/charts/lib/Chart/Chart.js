1e405f0511e3cd9b381ea982c777f15f8fc107c9
Anders Jonsson <aj224rj@student.lnu.se>
2022-10-13 12:14:18 +0200

    Refactor to explain myself in the code

--- package/src/charts/lib/Chart/Chart.js
+++ package/src/charts/lib/Chart/Chart.js
@@ -2,9 +2,8 @@
  * Custom chart element.
  *
  * @author Anders Jonsson
- * @version 1.0.0
+ * @version 1.0.1
  */
-import { StatsCollection } from '../../../stats/StatsCollection.js'
 import '../chart-canvas/index.js'
 import { COLORS } from '../constants/colors.js'
 import '../meta-data/index.js'
@@ -49,10 +48,6 @@ customElements.define(
      */
     #metaContainerElement
 
-    /**
-     * Create instance of class and attach open shadow-dom.
-     *
-     */
     constructor () {
       super()
 
@@ -60,58 +55,37 @@ customElements.define(
         template.content.cloneNode(true)
       )
 
+      this.#addShadowRootReferencesToClass()
+    }
+
+    #addShadowRootReferencesToClass () {
       this.#canvasElement = this.shadowRoot.querySelector('#canvas')
       this.#metaContainerElement = this.shadowRoot.querySelector('#meta-container')
     }
 
-    /**
-     * Attribute-names to observe and react on.
-     *
-     * @readonly
-     * @static
-     * @returns {string[]} - Array of attribute-names.
-     */
-    static get observedAttributes () {
-      return ['type']
-    }
-
-    /**
-     * Send data to chart-canvas to draw a pie-chart in canvas and display optional meta-data.
-     *
-     * @param {StatsCollection} statsCollection - StatsCollection-object.
-     * @param {object} options - Options-object.
-     */
     createPieChart (statsCollection, options) {
-      if (options?.title || options?.percent || options?.value) {
+      if (this.#toShowMetadata(options)) {
         this.#appendMetaBar(statsCollection, options)
       }
 
       this.#canvasElement.drawPieChart(statsCollection)
     }
 
-    /**
-     * Send data to chart-canvas to draw a bar-chart in canvas and display optional meta-data.
-     *
-     * @param {StatsCollection} statsCollection - StatsCollection-object.
-     * @param {object} options - Options-object.
-     */
     createBarChart (statsCollection, options) {
-      if (options?.title || options?.percent || options?.value) {
+      if (this.#toShowMetadata(options)) {
         this.#appendMetaBar(statsCollection, options)
       }
 
       this.#canvasElement.drawBarChart(statsCollection, options.average)
     }
 
-    /**
-     * Append custom meta-bar element to shadow-dom.
-     *
-     * @param {StatsCollection} statsCollection - StatsCollection-object.
-     * @param {object} options - Options-object.
-     */
-    async #appendMetaBar (statsCollection, options) {
+    #toShowMetadata (options) {
+      return options?.title || options?.percent || options?.value || options?.average
+    }
+
+    #appendMetaBar (statsCollection, options) {
       const metaBarElement = document.createElement('anjson-meta-bar')
-      const metaDataElements = await this.#generateMetaDataElements(statsCollection.getCollectionOfDataWithPercent(), options)
+      const metaDataElements = this.#generateMetaDataElements(statsCollection.getCollectionOfDataWithPercent(), options)
 
       for (const element of metaDataElements) {
         metaBarElement.appendChild(element)
@@ -120,14 +94,7 @@ customElements.define(
       this.#metaContainerElement.appendChild(metaBarElement)
     }
 
-    /**
-     * Append custom meta-bar element to shadow-dom.
-     *
-     * @param {object[]} dataCollection - Collection of data from StatsCollection, including the percent-property.
-     * @param {object} options - Options-object.
-     * @returns {HTMLElement[]} - Array of custom anjson-meta-data elements.
-     */
-    async #generateMetaDataElements (dataCollection, options) {
+    #generateMetaDataElements (dataCollection, options) {
       const metaDataElements = []
 
       for (const [index, data] of dataCollection.entries()) {
